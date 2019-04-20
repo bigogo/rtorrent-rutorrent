76a1abd751db407ff08f00c345b5e94fad487d99
mickkael <19755421+mickkael@users.noreply.github.com>
2019-04-20 17:46:19 +0800

    Update config.php

--- latest-raspbian/config/config.php
+++ latest-raspbian/config/config.php
@@ -28,11 +28,13 @@
 	// $scgi_host = "unix:///tmp/rpc.socket";
 	$XMLRPCMountPoint = "/RPC2";		// DO NOT DELETE THIS LINE!!! DO NOT COMMENT THIS LINE!!!
 	$pathToExternals = array(
-		"php" 	=> '',			// Something like /usr/bin/php. If empty, will be found in PATH.
+		"php" 	=> '/usr/bin/php',	// Something like /usr/bin/php. If empty, will be found in PATH.
 		"curl"	=> '/usr/bin/curl',	// Something like /usr/bin/curl. If empty, will be found in PATH.
 		"gzip"	=> '',			// Something like /usr/bin/gzip. If empty, will be found in PATH.
 		"id"	=> '',			// Something like /usr/bin/id. If empty, will be found in PATH.
 		"stat"	=> '/usr/bin/stat',	// Something like /usr/bin/stat. If empty, will be found in PATH.
+		"pgrep"	=> '/usr/bin/pgrep',	// Something like /usr/bin/pgrep. If empty, will be found in PATH.
+		"python"	=> '/usr/bin/python',	// Something like /usr/bin/python. If empty, will be found in PATH.
 	);
 	$localhosts = array( 			// list of local interfaces
 		"127.0.0.1", 
